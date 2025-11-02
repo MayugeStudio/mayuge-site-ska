5de010036c24c1eae520ed9c3966e48157ea13c2
MayugeStudio <my.favorite.is.energy@gmail.com>
2025-11-02 09:54:45 +0900

    Fix Home page duplication

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,4 @@
 <template>
-  <main class="p-6">
-    <h1>My Blog</h1>
-    <ul>
-      <li><router-link to="/post/hello">Hello</router-link></li>
-      <li><router-link to="/post/vue-tips">Vue Tips</router-link></li>
-    </ul>
-    <router-view />
-  </main>
+  <router-view />
 </template>
 
